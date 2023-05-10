1d02bdadd9599b427e33acd2037a4f04d0b9c658
Lukas Plank <lupl@tuta.io>
2023-05-10 08:30:12 +0200

    Moved examples.py to tests

---
+++ tests/example.py
@@ -0,0 +1,36 @@
+import pandas as pd
+from rdfdf import DFGraphConverter
+from rdflib import Namespace, Literal, Graph, URIRef
+from rdflib.namespace import FOAF, RDF
+
+example = Namespace("http://example.org/")
+
+def name_rule(obj_value):
+    graph = Graph()
+    
+    graph.add(
+        (subject, FOAF.name, Literal(obj_value))
+    )
+    
+    graph.add(
+        (subject, RDF.type, FOAF.Person)
+    )
+
+    return graph
+
+
+test_field_rules = {
+    "Name": name_rule,
+}
+
+df = pd.read_csv("./test_data/test.csv", sep=";")
+
+dfgraph = DFGraphConverter(
+    dataframe=df,
+    subject_column="Name",
+    subject_rule=example,
+    field_rules=test_field_rules,
+)
+
+graph = dfgraph.to_graph()
+print(graph.serialize(format="ttl"))
