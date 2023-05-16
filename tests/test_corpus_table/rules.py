c8f881967990a54ed13fa37459627dd409a98f8b
Lukas Plank <lupl@tuta.io>
2023-05-16 17:35:42 +0200

    Added corpus_link rule

--- tests/test_corpus_table/rules.py
+++ tests/test_corpus_table/rules.py
@@ -5,6 +5,7 @@ from rdflib import URIRef, Literal, Graph
 from rdflib.namespace import Namespace, RDF
 
 CRM = Namespace("http://www.cidoc-crm.org/cidoc-crm#")
+CRMCLS = Namespace("https://clscor.io/ontologies/CRMcls/")
 
 def rule_corpus_name():
     
@@ -51,3 +52,56 @@ def rule_corpus_name():
 
     return graph
 
+
+
+def _rule_corpus_link_corpus_name():
+    """corpusName rule for rule_corpus_link
+    """
+
+    __store__["corpus_name"] = __subject__
+
+    
+def rule_corpus_link():
+    
+    subject = __subject__.lower()
+
+    triples = [
+        (
+            URIRef("https://core.clscor.io/entity/type/linkType/corpus-website"),
+            CRM.P2i_is_type_of,
+            URIRef(__object__)
+        ),
+        (
+            URIRef(f"https://{subject}.clscor.io/entity/corpus"),
+            RDF.type,
+            CRMCLS.X1_Corpus
+        ),
+        (
+            URIRef(f"https://{subject}.clscor.io/entity/corpus"),
+            CRM.P1_is_identified_by,
+            URIRef(__object__)
+        ),
+        (
+            URIRef(__object__),
+            CRM.P1i_identifies,
+            URIRef(f"https://{subject}.clscor.io/entity/corpus"),
+        ),
+        (
+            URIRef(__object__),
+            CRM.P2_has_type,
+            URIRef("https://core.clscor.io/entity/type/linkType/corpus-website"),
+        ),
+        (
+            URIRef(__object__),
+            RDF.value,
+            Literal(f"Link to the {__store__['corpus_name']} website.")
+        )
+        
+    ]
+
+    graph = Graph()
+
+    for triple in triples:
+        graph.add(triple)
+
+    return graph
--- tests/test_corpus_table/test_corpus_table.py
+++ tests/test_corpus_table/test_corpus_table.py
@@ -9,7 +9,8 @@ import rdfdf
 import helpers.importers
 
 from table_partitions import (
-    rem_partition
+    rem_partition,
+    greekdracor_partition
 )
 
 # for custom importer see helpers.importers
@@ -26,7 +27,8 @@ from rules import (
     # rule_corpus_acronym,
     # rule_corpus_api,
     # rule_corpus_license,
-    # rule_corpus_link,
+    rule_corpus_link,
+    _rule_corpus_link_corpus_name,
     rule_corpus_name,
     # rule_corpus_language # ?
 )
@@ -54,7 +56,23 @@ def test_corpus_link():
     """rdfdf conversion test.
     Checks for graph isomorphism between an rdfdf-generated graph and a target graph.
     """
-    pass
+
+    _actual_conversion = rdfdf.DFGraphConverter(
+        dataframe=greekdracor_partition,
+        subject_column="corpusAcronym",
+        column_rules={
+            "corpusName": _rule_corpus_link_corpus_name,
+            "corpusLink": rule_corpus_link
+        }
+    )
+
+    actual_corpus_link = _actual_conversion.to_graph()
+
+    assert isomorphic(actual_corpus_link, target_corpus_link)
+
+# still AssertionError -> fix rule
+test_corpus_link()
+    
 
 def test_corpus_name():
     """rdfdf conversion test.
@@ -64,9 +82,12 @@ def test_corpus_name():
     _actual_conversion = rdfdf.DFGraphConverter(
         dataframe=rem_partition,
         subject_column="corpusAcronym",
-        column_rules={"corpusName": rule_corpus_name},
+        column_rules={"corpusName": rule_corpus_name}
     )
-
+    
     actual_corpus_name = _actual_conversion.to_graph()
 
     assert isomorphic(actual_corpus_name, target_corpus_name)
+
+
+
